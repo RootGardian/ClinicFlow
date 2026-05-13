888cea721d87da3ee213315bcd1950f47a6f872b
SatanVI <Ahmedbangoura852@gmail.com>
2026-05-13 16:04:18 +0100

    Update

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { register, login, getMe, getNotifications, markNotificationAsRead, updateAvatar } = require('../controllers/authController');
+const { register, login, getMe, getNotifications, markNotificationAsRead, updateAvatar, forgotPassword, resetPassword } = require('../controllers/authController');
 const { protect } = require('../middlewares/authMiddleware');
 const { registerValidationRules, validate } = require('../middlewares/validator');
 const upload = require('../middlewares/uploadMiddleware');
@@ -11,5 +11,7 @@ router.get('/me', protect, getMe);
 router.get('/notifications', protect, getNotifications);
 router.put('/notifications/:id', protect, markNotificationAsRead);
 router.put('/avatar', protect, upload.single('avatar'), updateAvatar);
+router.post('/forgot-password', forgotPassword);
+router.post('/reset-password', resetPassword);
 
 module.exports = router;
